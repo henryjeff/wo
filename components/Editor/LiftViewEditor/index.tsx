9331667f1ca96f7733b974d21a2d32461b730908
henryjeff <henryheffernan@gmail.com>
2022-06-29 05:39:38 -0400

    reduce this

--- components/Editor/LiftViewEditor/index.tsx
+++ components/Editor/LiftViewEditor/index.tsx
@@ -1,56 +1,134 @@
-import { useState } from "react";
+import { faAdd, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useCallback, useEffect, useState } from "react";
+import { EditorActions, EditorState } from "../../../pages/editor";
 import styles from "./LiftViewEditor.module.css";
 
-type LiftViewEditorProps = {};
+type LiftViewEditorProps = {
+  index: number;
+  editorState: EditorState;
+  editorDispatch: React.Dispatch<EditorActions>;
+};
 
-const LiftViewEditor: React.FC<LiftViewEditorProps> = ({}) => {
-  const [liftSets, setLiftSets] = useState<LiftSet[]>([]);
-  const [liftName, setLiftName] = useState("Bench Press");
+export type InputLiftSet = {
+  numSets: string;
+  numReps: string;
+  weight: string;
+  weightUnit: WeightUnit;
+};
 
-  const handleLiftInput = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    key: "numReps" | "numSets" | "weight",
-    liftIndex: number
-  ) => {
-    const newLiftSets = [...liftSets];
-    let newValue = Number(e.target.value);
-    newLiftSets[liftIndex][key] = newValue;
-    setLiftSets(newLiftSets);
-  };
+const LiftViewEditor: React.FC<LiftViewEditorProps> = ({
+  index,
+  editorState,
+  editorDispatch,
+}) => {
+  const { name, sets } = editorState[index];
 
-  const handleLiftNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLiftName(e.target.value);
-  };
+  const handleChangeNumSets = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>, setIndex: number) => {
+      editorDispatch({
+        type: "UPDATE_LIFT_SET_NUM_SETS",
+        liftIndex: index,
+        setIndex,
+        numSets: e.target.value,
+      });
+    },
+    [index, editorDispatch]
+  );
 
-  const createNewSet = () => {
-    setLiftSets([
-      ...liftSets,
-      {
-        numReps: 0,
-        numSets: 0,
-        weight: 0,
-        weightUnit: "lbs",
-      },
-    ]);
-  };
+  const handleChangeNumReps = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>, setIndex: number) => {
+      editorDispatch({
+        type: "UPDATE_LIFT_SET_NUM_REPS",
+        liftIndex: index,
+        setIndex,
+        numReps: e.target.value,
+      });
+    },
+    [index, editorDispatch]
+  );
+
+  const handleChangeWeight = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>, setIndex: number) => {
+      editorDispatch({
+        type: "UPDATE_LIFT_SET_WEIGHT",
+        liftIndex: index,
+        setIndex,
+        weight: e.target.value,
+      });
+    },
+    [index, editorDispatch]
+  );
+
+  const handleLiftNameChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      editorDispatch({
+        type: "UPDATE_LIFT_NAME",
+        liftIndex: index,
+        liftName: e.target.value,
+      });
+    },
+    [index, editorDispatch]
+  );
+
+  const handleDeleteSet = useCallback(
+    (setIndex: number) => {
+      editorDispatch({
+        type: "DELETE_LIFT_SET",
+        liftIndex: index,
+        setIndex,
+      });
+    },
+    [index, editorDispatch]
+  );
+
+  const createNewSet = useCallback(() => {
+    editorDispatch({
+      type: "ADD_LIFT_SET",
+      liftIndex: index,
+    });
+  }, [index, editorDispatch]);
+
+  const handleDeleteLift = useCallback(() => {
+    editorDispatch({
+      type: "DELETE_LIFT",
+      liftIndex: index,
+    });
+  }, [index, editorDispatch]);
 
   return (
     <div className={styles.container}>
       <div className={styles.liftNameRow}>
-        <input type="text" value={liftName} onChange={handleLiftNameChange} />
-        <p>Delete Lift</p>
+        <div>
+          <span
+            className={styles.liftNameInput}
+            role="textbox"
+            onChange={handleLiftNameChange}
+            contentEditable
+            placeholder="Lift Name"
+            suppressContentEditableWarning={true}
+          >
+            {name}
+          </span>
+        </div>
+        <div className={styles.deleteLiftButton} onClick={handleDeleteLift}>
+          <FontAwesomeIcon
+            icon={faXmark}
+            className={styles.deleteLiftButtonIcon}
+          />
+          <p>Delete</p>
+        </div>
       </div>
       <table className={styles.liftSetTable}>
         <tbody>
-          {liftSets.map((set, i) => {
+          {sets.map((set, i) => {
             return (
               <tr key={i} className={styles.row}>
                 <td className={styles.col}>
                   <input
                     type="text"
-                    style={{ maxWidth: 64 }}
-                    value={set.numSets === 0 ? "" : set.numSets}
-                    onChange={(e) => handleLiftInput(e, "numSets", i)}
+                    value={set.numSets}
+                    onChange={(e) => handleChangeNumSets(e, i)}
                   />
                 </td>
                 <td className={styles.col}>
@@ -59,9 +137,8 @@ const LiftViewEditor: React.FC<LiftViewEditorProps> = ({}) => {
                 <td className={styles.col}>
                   <input
                     type="text"
-                    style={{ maxWidth: 64 }}
-                    value={set.numReps === 0 ? "" : set.numReps}
-                    onChange={(e) => handleLiftInput(e, "numReps", i)}
+                    value={set.numReps}
+                    onChange={(e) => handleChangeNumReps(e, i)}
                   />
                 </td>
                 <td className={styles.col}>
@@ -70,20 +147,35 @@ const LiftViewEditor: React.FC<LiftViewEditorProps> = ({}) => {
                 <td className={styles.col}>
                   <input
                     type="text"
-                    style={{ maxWidth: 64 }}
-                    value={set.weight === 0 ? "" : set.weight}
-                    onChange={(e) => handleLiftInput(e, "weight", i)}
+                    value={set.weight}
+                    onChange={(e) => handleChangeWeight(e, i)}
                   />
                 </td>
                 <td className={styles.col}>
                   <p>lbs</p>
+                </td>
+                {/* <td className={styles.col}></td> */}
+                <td
+                  className={styles.col}
+                  style={{ paddingLeft: "3em" }}
+                  onClick={() => handleDeleteSet(i)}
+                >
+                  <FontAwesomeIcon
+                    icon={faXmark}
+                    className={styles.deleteSetIcon}
+                  />
                 </td>
               </tr>
             );
           })}
         </tbody>
       </table>
-      <p onClick={createNewSet}>+ Add Set</p>
+      <div className={styles.liftFooter}>
+        <div onClick={createNewSet} className={styles.addSetButton}>
+          <FontAwesomeIcon icon={faAdd} />
+          <p>Add Set</p>
+        </div>
+      </div>
     </div>
   );
 };
--- pages/editor/index.tsx
+++ pages/editor/index.tsx
@@ -1,11 +1,205 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useReducer } from "react";
 import { Layout, DefaultHeadMetaTags } from "../../components";
 import Badge from "../../components/Badge";
-import LiftViewEditor from "../../components/Editor/LiftViewEditor";
+import LiftViewEditor, {
+  InputLiftSet,
+} from "../../components/Editor/LiftViewEditor";
 import styles from "./Editor.module.css";
 
+export type EditorState = InputLift[];
+
+type InputLift = {
+  name: string;
+  sets: InputLiftSet[];
+};
+
+export type EditorActions =
+  | {
+      type: "ADD_LIFT";
+      lift: InputLift;
+    }
+  | {
+      type: "DELETE_LIFT";
+      liftIndex: number;
+    }
+  | {
+      type: "UPDATE_LIFT_NAME";
+      liftName: string;
+      liftIndex: number;
+    }
+  | {
+      type: "ADD_LIFT_SET";
+      liftIndex: number;
+    }
+  | {
+      type: "DELETE_LIFT_SET";
+      liftIndex: number;
+      setIndex: number;
+    }
+  | {
+      type: "UPDATE_LIFT_SET_NUM_REPS";
+      liftIndex: number;
+      setIndex: number;
+      numReps: string;
+    }
+  | {
+      type: "UPDATE_LIFT_SET_NUM_SETS";
+      liftIndex: number;
+      setIndex: number;
+      numSets: string;
+    }
+  | {
+      type: "UPDATE_LIFT_SET_WEIGHT";
+      liftIndex: number;
+      setIndex: number;
+      weight: string;
+    }
+  | {
+      type: "UPDATE_LIFT_SET_WEIGHT_UNIT";
+      liftIndex: number;
+      setIndex: number;
+      weightUnit: WeightUnit;
+    };
+
 const Home: NextPage = () => {
+  const [state, dispatch] = useReducer(
+    (state: EditorState, action: EditorActions) => {
+      switch (action.type) {
+        case "ADD_LIFT":
+          return [...state, action.lift];
+        case "DELETE_LIFT":
+          return state.filter((_, index) => index !== action.liftIndex);
+        case "ADD_LIFT_SET":
+          return state.map((lift, index) => {
+            if (index === action.liftIndex) {
+              const initLift = {
+                numReps: "",
+                numSets: "",
+                weight: "",
+                weightUnit: "lbs" as WeightUnit,
+              };
+              return {
+                ...lift,
+                sets: [...lift.sets, initLift],
+              };
+            }
+            return lift;
+          });
+        case "DELETE_LIFT_SET":
+          return state.map((lift, index) => {
+            if (index === action.liftIndex) {
+              return {
+                ...lift,
+                sets: lift.sets.filter(
+                  (_, setIndex) => setIndex !== action.setIndex
+                ),
+              };
+            }
+            return lift;
+          });
+
+        case "UPDATE_LIFT_NAME":
+          return state.map((lift, index) => {
+            if (index === action.liftIndex) {
+              return {
+                ...lift,
+                name: action.liftName,
+              };
+            }
+            return lift;
+          });
+        case "UPDATE_LIFT_SET_NUM_REPS":
+          return state.map((lift, liftIndex) => {
+            if (liftIndex === action.liftIndex) {
+              return {
+                ...lift,
+                sets: lift.sets.map((set, setIndex) => {
+                  if (setIndex === action.setIndex) {
+                    return {
+                      ...set,
+                      numReps: action.numReps,
+                    };
+                  }
+                  return set;
+                }),
+              };
+            }
+            return lift;
+          });
+        case "UPDATE_LIFT_SET_NUM_SETS":
+          return state.map((lift, liftIndex) => {
+            if (liftIndex === action.liftIndex) {
+              return {
+                ...lift,
+                sets: lift.sets.map((set, setIndex) => {
+                  if (setIndex === action.setIndex) {
+                    return {
+                      ...set,
+                      numSets: action.numSets,
+                    };
+                  }
+                  return set;
+                }),
+              };
+            }
+            return lift;
+          });
+        case "UPDATE_LIFT_SET_WEIGHT":
+          return state.map((lift, liftIndex) => {
+            if (liftIndex === action.liftIndex) {
+              return {
+                ...lift,
+                sets: lift.sets.map((set, setIndex) => {
+                  if (setIndex === action.setIndex) {
+                    return {
+                      ...set,
+                      weight: action.weight,
+                    };
+                  }
+                  return set;
+                }),
+              };
+            }
+            return lift;
+          });
+        case "UPDATE_LIFT_SET_WEIGHT_UNIT":
+          return state.map((lift, liftIndex) => {
+            if (liftIndex === action.liftIndex) {
+              return {
+                ...lift,
+                sets: lift.sets.map((set, setIndex) => {
+                  if (setIndex === action.setIndex) {
+                    return {
+                      ...set,
+                      weightUnit: action.weightUnit,
+                    };
+                  }
+                  return set;
+                }),
+              };
+            }
+            return lift;
+          });
+
+        default:
+          return state;
+      }
+    },
+    []
+  );
+
+  const handleAddLift = () => {
+    dispatch({
+      type: "ADD_LIFT",
+      lift: {
+        name: "",
+        sets: [],
+      },
+    });
+  };
+
   return (
     <>
       <Head>
@@ -14,7 +208,24 @@ const Home: NextPage = () => {
       <Layout>
         <div className={styles.layout}>
           {/* <h1>Editor</h1> */}
-          <LiftViewEditor />
+          {/* <LiftViewEditor
+            editorState={state}
+            editorDispatch={dispatch}
+            index={0}
+          /> */}
+          <div style={{ flexDirection: "column", flex: 1 }}>
+            {state.map((_, index) => (
+              <LiftViewEditor
+                key={`lift-${index}`}
+                editorState={state}
+                editorDispatch={dispatch}
+                index={index}
+              />
+            ))}
+            <span>
+              <button onClick={handleAddLift}>Create Lift</button>
+            </span>
+          </div>
         </div>
       </Layout>
     </>
