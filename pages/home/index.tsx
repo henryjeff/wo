64b717dc17afe7b0caf88d98d5daf3cafd4c4791
henryjeff <henryheffernan@gmail.com>
2022-07-01 00:00:48 -0400

    fix

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -66,7 +66,7 @@ const Home: NextPage = () => {
               />
             </AnimatedCanvasWrapper>
             <motion.div
-              {...mountAnimProps(0.25)}
+              {...mountAnimProps(0)}
               className={styles.heroOverlayContainer}
             >
               <div className={styles.heroOverlayShadow} />
