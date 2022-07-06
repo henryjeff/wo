a614d3bcbf699a293f56e3d749b1143c19827f55
henryjeff <henryheffernan@gmail.com>
2022-07-06 00:06:22 -0400

    fixed mongo

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -11,8 +11,6 @@ if (!uri) {
 }
 
 if (process.env.NODE_ENV === "development") {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
   // @ts-ignore
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
